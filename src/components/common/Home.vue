57902c50e53ca13399d2d4f11f9cd3cda1471805
dingbenfa <1406731195@qq.com>
2018-03-15 20:51:57 +0800

    first commit

---
+++ src/components/common/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="wrapper">
+        <v-head></v-head>
+        <v-sidebar></v-sidebar>
+        <v-tags></v-tags>
+        <div class="content">
+            <transition name="move" mode="out-in"><router-view></router-view></transition>
+        </div>
+    </div>
+</template>
+
+<script>
+    import vHead from './Header.vue';
+    import vSidebar from './Sidebar.vue';
+    import vTags from './Tags.vue';
+    export default {
+        components:{
+            vHead, vTags,vSidebar
+        }
+    }
+</script>
